7e5a8835980385f07372b55ed7cb618ce67c39af
shunific <139686386+shunific@users.noreply.github.com>
2025-05-01 17:26:01 +0800

    User login layout with empty dashboard

---
+++ InventoryGrocery/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryGrocery
+{
+
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void DashboardForm(object sender, EventArgs e)
+        {
+
+        }
+        private void LoginForm(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ InventoryGrocery/LoginForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryGrocery
+{
+    public partial class LoginForm : Form
+    {
+        private TextBox usernameTextBox;
+        private TextBox passwordTextBox;
+        private Button loginButton;
+        private Label userLabel;
+        private Label passLabel;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+       private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (username == "admin" && password == "admin123")
+            {
+                MessageBox.Show("Login successful.");
+                this.Hide();
+                DashboardForm dashboard = new DashboardForm("Admin");
+                dashboard.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid admin credentials.");
+            }
+        }
+    }
+}
